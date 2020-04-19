b2d311d2e0d25940f0c1f126107c4f3322fd96b3
wisnu <wisnus@linkaja.io>
2020-04-19 09:50:55 +0700

    Initial Commit

---
+++ YusMuvis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YusMuvis
+//
+//  Created by Wisnu Sanjaya on 19/04/20.
+//  Copyright © 2020 Wisnu Sanjaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
